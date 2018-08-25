ce7af4fc1f57da1ba5cd80c6c1fc5c5a3c4434ff
basystka <koleg379@gmail.com>
2018-08-25 16:11:29 +0300

    qwerty

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,18 +2,52 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
+	/**
+     * @Route("/contact", name="create-contact")
+     */
+	public function create(Request $request)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $contact = new Contact();
+            $contact->setName($data->getName());
+            $contact->setEmail($data->getEmail());
+            $contact->setPhone($data->getPhone());
+            $contact->setMessage($data->getMessage());
+
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('contact-send');
+        }
+
+        return $this->render('contact/create.html.twig', [
+            'contact_form' => $form->createView()
+        ]);
+    }
+
+
+
     /**
-     * @Route("/contact", name="contact")
+     * @Route("/contact-send", name="contact-send")
      */
     public function index()
     {
-        return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
-        ]);
+        return $this->render('contact/index.html.twig');
     }
 }
